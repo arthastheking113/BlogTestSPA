ace0881c215047b7ac80b3c8e9f6f67228eed993
arthas <arthastheking113@gmail.com>
2021-04-21 00:27:41 -0500

    update category service

--- src/app/shared/category.service.ts
+++ src/app/shared/category.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Category} from './category.model'
 import {environment} from 'src/environments/environment';
+import { ProgressbarService } from 'src/app/shared/services/progressbar.service';
+import { AlertService } from 'ngx-alerts';
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+    private progressService: ProgressbarService,
+    private alertService: AlertService) { }
   readonly baseUrl = `${environment.baseUrl}/Category`;
 
   list:Category[];
@@ -18,8 +22,21 @@ export class CategoryService {
   }
 
   async getCategoryPost(){
+    const categoryObserver = {
+      next: (x: any) => {
+        this.progressService.setSuccess();
+        this.list = x as Category[];  
+        this.progressService.completeLoading();
+      },
+      error: (err: any) => {
+        this.progressService.setFailure();
+       
+        console.log(err);
+        this.alertService.danger(err.error);
+        this.progressService.completeLoading();
+      },
+    };
     return await this.http.get(`${this.baseUrl}/getcategory`)
-    .toPromise()
-    .then(res => this.list = res as Category[]);
+    .subscribe(categoryObserver)
   }
 }
